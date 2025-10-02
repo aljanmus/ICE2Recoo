d00f0b76f39eae646c5ba7e0562d1ca8e3b37731
Mustafa aljanabi <aljanmus@sheridancollege.ca>
2025-10-02 09:40:27 -0400

    rev 1 local

---
+++ src/main/java/git/ice2_testgit/ICE2_testGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.ice2_testgit;
+
+/**
+ *
+ * @author musta
+ */
+public class ICE2_testGit {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1 local");
+    }
+}
